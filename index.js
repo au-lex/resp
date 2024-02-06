6fef7791740ac6d747c8b023db386c9140eef5fb
Aulex <117493012+au-lex@users.noreply.github.com>
2024-02-06 15:27:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.json());
    app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://joombow-web-application.vercel.app');
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
